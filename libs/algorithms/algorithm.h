fc294cf07510ea498b91b0b19a57756124e9e81a
slavikmotor <162026364+slavikmotor@users.noreply.github.com>
2024-03-17 11:35:47 +0300

    algorithm

---
+++ libs/algorithms/algorithm.h
@@ -0,0 +1,8 @@
+//
+// Created by Компьютер on 17.03.2024.
+//
+
+#ifndef UNTITLED1_ALGORITHM_H
+#define UNTITLED1_ALGORITHM_H
+
+#endif //UNTITLED1_ALGORITHM_H
---
+++ libs/algorithms/array/array.h
@@ -0,0 +1,8 @@
+//
+// Created by Компьютер on 17.03.2024.
+//
+
+#ifndef UNTITLED1_ARRAY_H
+#define UNTITLED1_ARRAY_H
+
+#endif //UNTITLED1_ARRAY_H
